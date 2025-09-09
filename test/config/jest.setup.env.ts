c05071360cc2b7c5961335d8052d5a4c6b4adce5
Guilherme Miranda <guilherme.souza@grupoimg.com.br>
2025-09-09 17:04:38 -0300

    Remove Docker configuration files and example tests to streamline the project

--- test/config/jest.setup.env.ts
+++
@@ -1,7 +0,0 @@
-import * as dotenv from "dotenv";
-
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-// Registry.getInstance().provide(
-//   "exampleRepository",
-//   ExampleRepository.getInstance()
-// );
